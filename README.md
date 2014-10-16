047a67a10639514db84fef709643945951bf1dd7
LaVigne, Scott <lavignes@amazon.com>
2014-10-16 15:13:51 -0700

    yo

---
+++ README.md
@@ -0,0 +1,264 @@
+#Chlorine<sub>2</sub>
+
+A more stable molecule.
+
+##What the heck is this thing?
+
+  * Chlorine is micro-framework for making C unit tests.
+
+  * It is meant to be easily dropped into existing C projects to facilitate unit testing.
+
+  * It aims to be [thread-safe](#threading) and incredibly simple to use.
+  
+##Features
+  
+  â˜‘ Test suites (Use chlorine bundles)
+
+  â˜‘ Tests run on seperate threads **(New in Cl<sub>2</sub>)**
+  
+  â˜ Test pooling
+  
+  â˜‘ Non-fatal assertions **(New in Cl<sub>2</sub>)**
+  
+  â˜‘ Re-entrant assertions **(New in Cl<sub>2</sub>)**
+  
+  â˜‘ Re-entrant Logging and timing **(New in Cl<sub>2</sub>)**
+
+  â˜ Test timeouts
+    
+  â˜ Test fixtures
+  
+##Quickstart
+
+1. First grab a copy of [chlorine.h](https://raw.githubusercontent.com/pyrated/chlorine/master/chlorine.h) and add it somewhere easily accessible in your project.
+
+2. Create a new file to house a test bundle. For example: "my_tests.c"
+ 
+    ```c
+    #include "chlorine.h"
+    
+    CL_SPEC (my_first_test) {
+        cl_log("This is my first empty test");
+    }
+    
+    CL_BUNDLE (
+        my_first_test,
+        /* I can add more specs here */
+    );
+    ```
+
+3. Compile & Run
+
+    ```sh
+    $ cc my_tests.c -o my_tests && ./my_tests
+    ```
+
+You see output similar to:
+
+```
+[INFO] Running BUNDLE: my_tests.c
+
+[INFO] Executing SPEC => my_first_test
+
+        [LOG]   This is my first empty test
+        [FAIL]  Failed SPEC in 0.0000 s -> 0 fail, 0 pass
+
+======================================================
+
+[FAILURE] 1 SPECS failed, 0 SPECS passed in 0.0001 s
+```
+  
+The test spec for my_first_test fails because it doesn't actually test for anything. If we want the test to blindly pass, we'll need to add a test condition. We can add conditions with **cl_assert()**:
+
+```c
+#include <stdbool.h>
+#include "chlorine.h"
+    
+CL_SPEC (my_first_test) {
+    cl_assert(true, "This will always pass");
+}
+
+CL_BUNDLE (
+    my_first_test,
+);
+```
+
+When we compile and run again we see:
+    
+```
+[INFO] Running BUNDLE: my_tests.c
+
+[INFO] Executing SPEC => my_first_test
+
+        [PASS]  Passed SPEC in 0.0000 s -> 0 fail, 1 pass
+
+======================================================
+
+[SUCCESS] 0 SPECS failed, 1 SPECS passed in 0.0002 s
+```
+
+You can test multiple conditions in a single test spec:
+
+```c
+#include <stdbool.h>
+#include "chlorine.h"
+
+CL_SPEC (my_first_test) {
+    cl_assert(true, "Always true");
+    cl_assert(false, "Always false");
+    cl_assert(2 + 2 == 5, "... still false");
+    cl_assert(true, "Finish on a high note!");
+}
+
+CL_BUNDLE (
+    my_first_test,
+);
+```
+
+output:
+
+```
+[INFO] Running BUNDLE: my_tests.c
+
+[INFO] Executing SPEC => my_first_test
+
+        [ERROR] Assertion Failed: false in my_tests.c:6
+                Always false
+
+        [ERROR] Assertion Failed: 2 + 2 == 5 in my_tests.c:7
+                ... still false
+
+        [FAIL]  Failed SPEC in 0.0000 s -> 2 fail, 2 pass
+
+======================================================
+
+[FAILURE] 1 SPECS failed, 0 SPECS passed in 0.0001 s
+```
+
+##Threading
+
+By default, Chlorine runs all tests serially. This is actually faster for many short and quick specs. But imagine you have tests like this: *(Take note of the order of the specs in the bundle)*
+
+```c
+#include <unistd.h>
+#include <stdbool.h>
+#include "chlorine.h"
+
+// We can call cl_assert outside of a spec
+// because it is thread-safe and re-entrant.
+void long_operation(int time) {
+  sleep(time);
+  cl_assert(true, "true");
+}
+
+CL_SPEC (fast_test) {
+    long_operation(0);
+}
+
+CL_SPEC (one_sec_test) {
+    long_operation(1);
+}
+
+CL_SPEC (two_sec_test) {
+    long_operation(2);
+}
+
+CL_BUNDLE (
+    two_sec_test,
+    fast_test,
+    one_sec_test,
+);
+```
+
+These specs take 3 seconds to run in serial:
+
+```
+[INFO] Running BUNDLE: my_tests.c
+
+[INFO] Executing SPEC => two_sec_test
+
+        [PASS]  Passed SPEC in 2.0011 s -> 0 fail, 1 pass
+
+======================================================
+
+[INFO] Executing SPEC => fast_test
+
+        [PASS]  Passed SPEC in 0.0000 s -> 0 fail, 1 pass
+
+======================================================
+
+[INFO] Executing SPEC => one_sec_test
+
+        [PASS]  Passed SPEC in 1.0010 s -> 0 fail, 1 pass
+
+======================================================
+
+[SUCCESS] 0 SPECS failed, 3 SPECS passed in 3.0026 s
+```
+
+But there are no issues with these specs mutating shared state (And there **never** should be). We can run these tests in parallel. Just change **CL_BUNDLE** to **CL_BUNDLE_PARALLEL**. So easy!
+
+```c
+#include <unistd.h>
+#include <stdbool.h>
+#include "chlorine.h"
+
+void long_operation(int time) {
+  sleep(time);
+  cl_assert(true, "true");
+}
+
+CL_SPEC (fast_test) {
+    long_operation(0);
+}
+
+CL_SPEC (one_sec_test) {
+    long_operation(1);
+}
+
+CL_SPEC (two_sec_test) {
+    long_operation(2);
+}
+
+CL_BUNDLE_PARALLEL (
+    two_sec_test,
+    fast_test,
+    one_sec_test,
+);
+```
+
+outputs:
+
+```
+[INFO] Running PARALLEL BUNDLE: my_tests.c
+
+[INFO] Executing SPEC => two_sec_test
+
+        [PASS]  Passed SPEC in 2.0005 s -> 0 fail, 1 pass
+
+======================================================
+
+[INFO] Executing SPEC => fast_test
+
+        [PASS]  Passed SPEC in 0.0000 s -> 0 fail, 1 pass
+
+======================================================
+
+[INFO] Executing SPEC => one_sec_test
+
+        [PASS]  Passed SPEC in 1.0011 s -> 0 fail, 1 pass
+
+======================================================
+
+[SUCCESS] 0 SPECS failed, 3 SPECS passed in 2.0007 s
+```
+
+The tests now only take as long as the slowest spec. All tests are running in parallel but **the logging order is still retained**! Each spec logs to thread-local storage and the log order is resolved in real time as the tests complete!
+
+##Other Stuff
+
+Im still working on this as I expand it for use in my own projects. It is actually now pretty usable.
+
+##Copyright & License
+
+Copyright (c) 2014 Scott LaVigne. See [LICENSE.txt](https://github.com/pyrated/chlorine/blob/master/LICENSE.txt) for details. 
---
+++ chlorine.h
@@ -0,0 +1,287 @@
+#ifndef CHLORINE2
+#define CHLORINE2
+
+#include <sys/time.h>
+
+#include <pthread.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define __CL_RESET "\x1B[0m"
+#define __CL_BOLD "\x1B[1m"
+#define __CL_UL "\x1B[4m"
+#define __CL_TAB "        "
+
+#define __CL_BLACK "0"
+#define __CL_RED "1"
+#define __CL_GREEN "2"
+#define __CL_YELLOW "3"
+#define __CL_BLUE "4"
+#define __CL_MAGENTA "5"
+#define __CL_CYAN "6"
+#define __CL_WHITE "7"
+#define __CL_FG(color) "\x1B[3"color"m"
+#define __CL_BG(color) "\x1B[4"color"m"
+
+pthread_key_t __CLEnvKey;
+
+typedef struct __CLSpecEnv {
+  bool failed;
+  char* test_name;
+  char* output_buffer;
+  size_t num_failed_asserts;
+  size_t num_passed_asserts;
+} __CLSpecEnv;
+
+typedef void* (*__CLSpecType)();
+
+void __cl_env_init(__CLSpecEnv* env) {
+  env->failed = false;
+  env->test_name = NULL;
+  env->output_buffer = malloc(16);
+  env->output_buffer = '\0';
+  env->num_failed_asserts = 0;
+  env->num_passed_asserts = 0;
+}
+
+__CLSpecEnv* __cl_env() {
+  return pthread_getspecific(__CLEnvKey);
+}
+
+void __cl_vappend(const char* format, va_list args) {
+  __CLSpecEnv *env = __cl_env();
+  size_t cursor = 0;
+  if (env->output_buffer) {
+    cursor = strlen(env->output_buffer);
+  }
+  char* new_output = NULL;
+  size_t new_output_size = vasprintf(&new_output, format, args);
+  env->output_buffer = realloc(env->output_buffer, cursor + new_output_size);
+  strncpy(env->output_buffer + cursor, new_output, new_output_size + 1);
+  free(new_output);
+}
+
+void __cl_append(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  __cl_vappend(format, args);
+  va_end(args);
+}
+
+void __cl_print(const char* format, ...) {
+  va_list args;
+  va_start (args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+}
+
+void __cl_info(const char* format, ...) {
+  __cl_append(__CL_BOLD __CL_FG(__CL_CYAN) "[INFO]" __CL_RESET " ");
+  va_list args;
+  va_start (args, format);
+  __cl_vappend(format, args);
+  va_end(args);
+}
+
+void __cl_error(const char* format, ...) {
+  __cl_append(__CL_BOLD __CL_FG(__CL_YELLOW) __CL_TAB "[ERROR]" __CL_RESET " ");
+  va_list args;
+  va_start (args, format);
+  __cl_vappend(format, args);
+  va_end(args);
+}
+
+void __cl_pass(const char* format, ...) {
+  __cl_append(__CL_BOLD __CL_FG(__CL_GREEN) __CL_TAB "[PASS] " __CL_RESET " ");
+  va_list args;
+  va_start (args, format);
+  __cl_vappend(format, args);
+  va_end(args);
+}
+
+void __cl_fail(const char* format, ...) {
+  __cl_append(__CL_BOLD __CL_FG(__CL_RED) __CL_TAB "[FAIL] " __CL_RESET " ");
+  va_list args;
+  va_start (args, format);
+  __cl_vappend(format, args);
+  va_end(args);
+}
+
+void __cl_log(const char* format, ...) {
+  __cl_append(__CL_BOLD __CL_FG(__CL_RED) __CL_TAB "[LOG]  " __CL_RESET " ");
+  va_list args;
+  va_start (args, format);
+  __cl_vappend(format, args);
+  va_end(args);
+  __cl_append("\n");
+}
+
+double __cl_time() {
+  struct timeval t; 
+  gettimeofday(&t, NULL);
+  return (double) t.tv_sec + t.tv_usec / 1000000.0;
+}
+
+#define cl_log(format, ...) __cl_log(format, ##__VA_ARGS__)
+
+#define cl_abort()                                                           \
+do {                                                                         \
+  __CLSpecEnv* env = __cl_env();                                             \
+  env->failed = true;                                                        \
+  __cl_fail("Aborted\n\n");                                                  \
+  __cl_append("======================================================\n\n"); \
+  pthread_exit(NULL);                                                        \
+} while(0);                                                                  \
+
+
+#define cl_assert(test, format, ...)                                         \
+do {                                                                         \
+  __CLSpecEnv* env = __cl_env();                                             \
+  if (!(test)) {                                                             \
+    env->num_failed_asserts++;                                               \
+    /* Log the assert failure */                                             \
+    __cl_error("Assertion Failed: " __CL_FG(__CL_RED) __CL_BOLD              \
+               "%s" __CL_RESET " in %s:%d\n" __CL_TAB __CL_TAB, #test,       \
+               __FILE__, __LINE__);                                          \
+    __cl_append(__CL_FG(__CL_BLUE));                                         \
+    __cl_append(format, ##__VA_ARGS__);                                      \
+    __cl_append(__CL_RESET "\n\n");                                          \
+  } else {                                                                   \
+    env->num_passed_asserts++;                                               \
+  }                                                                          \
+} while(0);                                                                  \
+
+#define CL_SPEC(name)                                                        \
+void __cl_spec_##name ();                                                    \
+void* name (void* data) {                                                    \
+  __CLSpecEnv* env = (__CLSpecEnv*) data;                                    \
+  env->test_name = #name;                                                    \
+  pthread_setspecific(__CLEnvKey, env);                                      \
+  __cl_info("Executing SPEC => " __CL_FG(__CL_CYAN) __CL_BOLD                \
+            "%s\n\n" __CL_RESET, #name);                                     \
+  double time_start = __cl_time();                                           \
+  __cl_spec_##name ();                                                       \
+  double elapsed = __cl_time() - time_start;                                 \
+  size_t passed = env->num_passed_asserts;                                   \
+  size_t failed = env->num_failed_asserts;                                   \
+  if (passed > 0 && failed == 0) {                                           \
+    __cl_pass("Passed SPEC in %.4f s -> " __CL_FG(__CL_RED) "%zu fail"       \
+      __CL_RESET ", " __CL_FG(__CL_GREEN) "%zu pass" __CL_RESET "\n\n",      \
+      elapsed, failed, passed);                                              \
+  } else {                                                                   \
+    env->failed = true;                                                      \
+    __cl_fail("Failed SPEC in %.4f s -> " __CL_FG(__CL_RED) "%zu fail"       \
+      __CL_RESET ", " __CL_FG(__CL_GREEN) "%zu pass" __CL_RESET "\n\n",      \
+      elapsed, failed, passed);                                              \
+  }                                                                          \
+  __cl_append("======================================================\n\n"); \
+  return NULL;                                                               \
+}                                                                            \
+void __cl_spec_##name ()                                                     \
+
+int __cl_main_parallel(
+  int argc,
+  char** argv,
+  __CLSpecType* specs,
+  size_t num_specs,
+  const char* filename)
+{
+  pthread_key_create(&__CLEnvKey, NULL);
+  size_t i;
+  size_t failed = 0;
+  double time_start = __cl_time();
+  __cl_print(__CL_BOLD __CL_FG(__CL_CYAN) "[INFO] " __CL_RESET
+             "Running PARALLEL BUNDLE: %s\n\n", filename);
+  /* TODO: Use a simple thread pool */
+  pthread_t* threads = malloc(sizeof(pthread_t) * num_specs);
+  __CLSpecEnv* envs = malloc(sizeof(__CLSpecEnv) * num_specs);
+  for (i = 0; i < num_specs; i++) {
+    __cl_env_init(&envs[i]);
+    pthread_create(&threads[i], NULL, specs[i], &envs[i]);
+  }
+  for (i = 0; i < num_specs; i++) {
+    pthread_join(threads[i], NULL);
+    __CLSpecEnv *env = &envs[i];
+    if (env->output_buffer) {
+      fprintf(stderr, "%s", env->output_buffer);
+      free(env->output_buffer);
+    }
+    if (env->failed) {
+      failed++;
+    }
+  }
+  free(threads);
+  free(envs);
+  size_t passed = num_specs - failed;
+  double elapsed = __cl_time() - time_start;
+  if (failed > 0) {
+    __cl_print(__CL_BOLD __CL_FG(__CL_RED) "[FAILURE] " __CL_RESET);
+  } else {
+    __cl_print(__CL_BOLD __CL_FG(__CL_GREEN) "[SUCCESS] " __CL_RESET);
+  }
+  __cl_print(__CL_FG(__CL_RED) "%zu SPECS failed" __CL_RESET ", "
+             __CL_FG(__CL_GREEN) "%zu SPECS passed" __CL_RESET
+            " in %.4f s\n\n", failed, passed, elapsed);
+  return failed;
+}
+
+int __cl_main(
+  int argc,
+  char** argv,
+  __CLSpecType* specs,
+  size_t num_specs,
+  const char* filename)
+{
+  pthread_key_create(&__CLEnvKey, NULL);
+  size_t i;
+  size_t failed = 0;
+  double time_start = __cl_time();
+  __cl_print(__CL_BOLD __CL_FG(__CL_CYAN) "[INFO] " __CL_RESET
+             "Running BUNDLE: %s\n\n", filename);
+  pthread_t thread;
+  __CLSpecEnv env;
+  for (i = 0; i < num_specs; i++) {
+    __cl_env_init(&env);
+    pthread_create(&thread, NULL, specs[i], &env);
+    pthread_join(thread, NULL);
+    if (env.output_buffer) {
+      fprintf(stderr, "%s", env.output_buffer);
+      free(env.output_buffer);
+      if (env.failed) {
+        failed++;
+      }
+    }
+  }
+  size_t passed = num_specs - failed;
+  double elapsed = __cl_time() - time_start;
+  if (failed > 0) {
+    __cl_print(__CL_BOLD __CL_FG(__CL_RED) "[FAILURE] " __CL_RESET);
+  } else {
+    __cl_print(__CL_BOLD __CL_FG(__CL_GREEN) "[SUCCESS] " __CL_RESET);
+  }
+  __cl_print(__CL_FG(__CL_RED) "%zu SPECS failed" __CL_RESET ", "
+             __CL_FG(__CL_GREEN) "%zu SPECS passed" __CL_RESET
+             " in %.4f s\n\n", failed, passed, elapsed);
+  return failed;
+}
+
+#define CL_BUNDLE_PARALLEL(...)                                              \
+int main(int argc, char** argv) {                                            \
+  __CLSpecType specs[] = {__VA_ARGS__};                                      \
+  size_t num_specs = sizeof(specs) / sizeof(__CLSpecType);                   \
+  return __cl_main_parallel(argc, argv, specs, num_specs, __FILE__);         \
+}                                                                            \
+
+#define CL_BUNDLE(...)                                                       \
+int main(int argc, char** argv) {                                            \
+  __CLSpecType specs[] = {__VA_ARGS__};                                      \
+  size_t num_specs = sizeof(specs) / sizeof(__CLSpecType);                   \
+  return __cl_main(argc, argv, specs, num_specs, __FILE__);                  \
+}                                                                            \
+
+#endif /* CHLORINE2 */
